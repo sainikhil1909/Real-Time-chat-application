99eef50503f2271c6b4ab6b7e341ca7a91bbff5e
Sai Nikhil <123650572+sainikhil1909@users.noreply.github.com>
2026-05-26 19:41:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname + "/main")));
-
+app.use(express.static(path.join(__dirname)));
 io.on("connection", function (socket) {
 
   socket.on("newuser", function (username) {
